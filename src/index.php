cd0f7a802cb9a14e1a0cefc47e2ef2ba4ea56b0b
Lopez Mark <leon51414@icloud.com>
2020-12-19 20:14:27 +0900

    section1完成

--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
 
+<div id="section1">
+        <div class="section1__titlebox">
+            <h2 class="section1__title">L.WEB DESIGN</h2>
+            <div class="section1__underline"></div>
+            <p class="section1__text">Milk tea is my energy source</p>
+        </div>
+        <div class="section1__mousebox">
+            <div class="section1__line"></div>
+            <div class="section1__mouseicon"></div>
+        </div>
+</div>
+
+
 <?php get_footer(); ?>
